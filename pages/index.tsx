ff6d292bcf241a2c296f4eb73b98a5e3523ba9d6
divlo <contact@divlo.fr>
2020-10-23 20:48:57 +0200

    fix: clean up homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,3 @@
-import { Button } from 'components/design/Button'
-import { Input } from 'components/design/Input'
 import Head from 'components/Head'
 
 const Home: React.FC = () => {
@@ -8,24 +6,6 @@ const Home: React.FC = () => {
       <Head />
 
       <p>Hello Thream! 👋</p>
-      <Button>Salut</Button>
-      <br />
-      <br />
-      <Button type='disabled'>Salut</Button>
-      <br />
-      <br />
-      <Button type='large'>Salut</Button>
-      <br />
-      <br />
-      <Button socialMediaType='discord'>Discord</Button>
-      <br />
-      <Button socialMediaType='twitter'>Twitter</Button>
-      <br />
-      <Button socialMediaType='google'>Google</Button>
-      <br />
-      <Button socialMediaType='github'>GitHub</Button>
-      <Input label='Name' name='name' type='text' />
-      <Input label='Password' name='password' type='password' state='togglePassword' />
 
       <style jsx>
         {`
