8e89ea8a6882131fae37a20f2f56c74d5426dfc2
Mouhamed27-99 <medsoseck@gmail.com>
2020-11-19 14:18:33 +0100

    fix: change tooltip props type and default props value

--- components/design/Tooltip.tsx
+++ components/design/Tooltip.tsx
@@ -3,23 +3,23 @@ import { forwardRef, useState } from 'react'
 import { ReactHTMLProps } from 'typings/utils'
 
 type Tooltip = ReactHTMLProps<HTMLDivElement> & {
-  content: 'string'
+  content: string
   direction?: 'top' | 'bottom' | 'right' | 'left'
   delay?: number
 }
 
 export const Tooltip = forwardRef<HTMLButtonElement, Tooltip>((props, ref) => {
-  const { direction = 'top', delay = 5, children, content, ...rest } = props
+  const { direction = 'bottom', delay = 5, children, content, ...rest } = props
 
   let timeout: ReturnType<typeof setTimeout>
   const [active, setActive] = useState(false)
 
-  const showTip = () => {
+  const showTip = (): void => {
     timeout = setTimeout(() => {
       setActive(true)
     }, delay)
   }
-  const hideTip = () => {
+  const hideTip = (): void => {
     clearInterval(timeout)
     setActive(false)
   }
@@ -27,6 +27,7 @@ export const Tooltip = forwardRef<HTMLButtonElement, Tooltip>((props, ref) => {
   return (
     <>
       <div
+        {...rest}
         className='Tooltip-Wrapper'
         onMouseEnter={showTip}
         onMouseLeave={hideTip}
@@ -35,85 +36,87 @@ export const Tooltip = forwardRef<HTMLButtonElement, Tooltip>((props, ref) => {
         {active && <div className={`Tooltip-Tip ${direction}`}>{content}</div>}
       </div>
 
-      <style jsx>{`
-        .Tooltip-Wrapper {
-          --tooltip-text-color: white;
-          --tooltip-background-color: black;
-          --tooltip-margin: 30px;
-          --tooltip-arrow-size: 6px;
-        }
-        .Tooltip-Wrapper {
-          display: inline-block;
-          position: relative;
-        }
-        .Tooltip-Tip {
-          position: absolute;
-          border-radius: 4px;
-          left: 50%;
-          transform: translateX(-50%);
-          padding: 6px;
-          color: var(--tooltip-text-color);
-          background: var(--tooltip-background-color);
-          font-size: 14px;
-          font-family: sans-serif;
-          line-height: 1;
-          z-index: 100;
-          white-space: nowrap;
-        }
+      <style jsx>
+        {`
+          .Tooltip-Wrapper {
+            --tooltip-text-color: white;
+            --tooltip-background-color: black;
+            --tooltip-margin: 30px;
+            --tooltip-arrow-size: 6px;
+          }
+          .Tooltip-Wrapper {
+            display: inline-block;
+            position: relative;
+          }
+          .Tooltip-Tip {
+            position: absolute;
+            border-radius: 4px;
+            left: 50%;
+            transform: translateX(-50%);
+            padding: 6px;
+            color: var(--tooltip-text-color);
+            background: var(--tooltip-background-color);
+            font-size: 14px;
+            font-family: sans-serif;
+            line-height: 1;
+            z-index: 100;
+            white-space: nowrap;
+          }
 
-        .Tooltip-Tip::before {
-          content: ' ';
-          left: 50%;
-          border: solid transparent;
-          height: 0;
-          width: 0;
-          position: absolute;
-          pointer-events: none;
-          border-width: var(--tooltip-arrow-size);
-          margin-left: calc(var(--tooltip-arrow-size) * -1);
-        }
+          .Tooltip-Tip::before {
+            content: ' ';
+            left: 50%;
+            border: solid transparent;
+            height: 0;
+            width: 0;
+            position: absolute;
+            pointer-events: none;
+            border-width: var(--tooltip-arrow-size);
+            margin-left: calc(var(--tooltip-arrow-size) * -1);
+          }
 
-        .Tooltip-Tip.top {
-          top: calc(var(--tooltip-margin) * -1);
-        }
-        .Tooltip-Tip.top::before {
-          top: 100%;
-          border-top-color: var(--tooltip-background-color);
-        }
+          .Tooltip-Tip.top {
+            top: calc(var(--tooltip-margin) * -1);
+          }
+          .Tooltip-Tip.top::before {
+            top: 100%;
+            border-top-color: var(--tooltip-background-color);
+          }
 
-        .Tooltip-Tip.right {
-          left: calc(100% + var(--tooltip-margin));
-          top: 50%;
-          transform: translateX(0) translateY(-50%);
-        }
-        .Tooltip-Tip.right::before {
-          left: calc(var(--tooltip-arrow-size) * -1);
-          top: 50%;
-          transform: translateX(0) translateY(-50%);
-          border-right-color: var(--tooltip-background-color);
-        }
-        .Tooltip-Tip.bottom {
-          bottom: calc(var(--tooltip-margin) * -1);
-        }
-        .Tooltip-Tip.bottom::before {
-          bottom: 100%;
-          border-bottom-color: var(--tooltip-background-color);
-        }
+          .Tooltip-Tip.right {
+            left: calc(100% + var(--tooltip-margin));
+            top: 50%;
+            transform: translateX(0) translateY(-50%);
+          }
+          .Tooltip-Tip.right::before {
+            left: calc(var(--tooltip-arrow-size) * -1);
+            top: 50%;
+            transform: translateX(0) translateY(-50%);
+            border-right-color: var(--tooltip-background-color);
+          }
+          .Tooltip-Tip.bottom {
+            bottom: calc(var(--tooltip-margin) * -1);
+          }
+          .Tooltip-Tip.bottom::before {
+            bottom: 100%;
+            border-bottom-color: var(--tooltip-background-color);
+          }
 
-        .Tooltip-Tip.left {
-          left: auto;
-          right: calc(100% + var(--tooltip-margin));
-          top: 50%;
-          transform: translateX(0) translateY(-50%);
-        }
-        .Tooltip-Tip.left::before {
-          left: auto;
-          right: calc(var(--tooltip-arrow-size) * -2);
-          top: 50%;
-          transform: translateX(0) translateY(-50%);
-          border-left-color: var(--tooltip-background-color);
-        }
-      `}</style>
+          .Tooltip-Tip.left {
+            left: auto;
+            right: calc(100% + var(--tooltip-margin));
+            top: 50%;
+            transform: translateX(0) translateY(-50%);
+          }
+          .Tooltip-Tip.left::before {
+            left: auto;
+            right: calc(var(--tooltip-arrow-size) * -2);
+            top: 50%;
+            transform: translateX(0) translateY(-50%);
+            border-left-color: var(--tooltip-background-color);
+          }
+        `}
+      </style>
     </>
   )
 })
